86e6d81cb71c36d44355a0459ed6cc0142738b4b
alok.kumar <alokr.mail@gmail.com>
2015-07-18 15:38:08 +0530

    Commit after plugin skeleten created

---
+++ db/access.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Capability  information
+ *
+ * @package   mod_appaer
+ * @copyright 2105 onwards Alok Kumar Rai alokr.mail@gmail.com
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$capabilities = array(
+  'mod/appear:addinstance' => array(
+        'riskbitmask' => RISK_XSS,
+
+        'captype' => 'write',
+        'contextlevel' => CONTEXT_COURSE,
+        'archetypes' => array(
+            'editingteacher' => CAP_ALLOW,
+            'manager' => CAP_ALLOW
+        ),
+        'clonepermissionsfrom' => 'moodle/course:manageactivities'
+    )
+    );
--- version.php
+++ version.php
@@ -1,8 +1,14 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * Version information
+ *
+ * @package   mod_appaer
+ * @copyright 2105 onwards Alok Kumar Rai alokr.mail@gmail.com
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
+defined('MOODLE_INTERNAL')||die();
+$plugin->version = 2015071700;
+$plugin->requires = 2013110400;
+$plugin->component = 'mod_appear';
